056d467b38ce4c615cfe79d1dc38e824cc3fa8a3
albertjan <albertjan@curit.com>
2014-10-21 23:06:02 +0200

    add error message

--- app.js
+++ app.js
@@ -11,12 +11,12 @@
     
     app.controller('LoginController', ['$scope', '$rootScope', '$location', '$http', function ($scope, $rootScope, $location, $http) {
         $scope.login = function() {
-            $http.post("https://ws.spotify.com/oauth/token", 
-                       { grant_type: "client_credentials", client_id: $scope.username, client_secret: $scope.password },
-                       { }
-            ).then(function (data) {
+            $http.get("https://ws.spotify.com/oauth/token?grant_type=client_credentials&client_id=" + $scope.username + "&client_secret=" + $scope.password)
+            .then(function (data) {
                 console.log(JSON.stringify(data));
                 $location.path("/");
+            }, function(data) {
+                $scope.errorMessage = data.data.error;
             });
         };
     }]);
